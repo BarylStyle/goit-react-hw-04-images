0a0fc1563ba32cc3d72b1dbb68ed5ff7cc92309f
BarylStyle <mateusz.consulting@gmail.com>
2024-06-24 11:33:50 +0200

    hw4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
@@ -9,73 +9,64 @@ import  "./Styles/app.scss";
 
 const API_KEY = '43679088-c0d84692f93956b11d66778fa';
 
-class App extends Component {
-  state = {
-    images: [],
-    loading: false,
-    error: null,
-    searchQuery: '',
-    page: 1,
-    showModal: false,
-    largeImageURL: '',
-  };
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    const { searchQuery, page } = this.state;
-
-    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
-      this.fetchImages();
-    }
-  }
-
-  fetchImages = () => {
-    const { searchQuery, page } = this.state;
-
-    this.setState({ loading: true });
+  const fetchImages = useCallback(() => {
+    setLoading(true);
 
     axios
       .get(
         `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
       )
       .then((response) => {
-        this.setState((prevState) => ({
-          images: [...prevState.images, ...response.data.hits],
-        }));
+        setImages((prevImages) => [...prevImages, ...response.data.hits]);
       })
-      .catch((error) => this.setState({ error }))
-      .finally(() => this.setState({ loading: false }));
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, [searchQuery, page]);
+
+  useEffect(() => {
+    if (searchQuery) {
+      fetchImages();
+    }
+  }, [searchQuery, page, fetchImages]);
+
+  const handleSearchSubmit = (query) => {
+    setSearchQuery(query);
+    setPage(1);
+    setImages([]);
   };
 
-  handleSearchSubmit = (query) => {
-    this.setState({ searchQuery: query, page: 1, images: [] });
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
   };
 
-  handleLoadMore = () => {
-    this.setState((prevState) => ({ page: prevState.page + 1 }));
+  const handleImageClick = (largeImageURL) => {
+    setLargeImageURL(largeImageURL);
+    setShowModal(true);
   };
 
-  handleImageClick = (largeImageURL) => {
-    this.setState({ largeImageURL, showModal: true });
+  const toggleModal = () => {
+    setShowModal((prevShowModal) => !prevShowModal);
   };
 
-  toggleModal = () => {
-    this.setState((prevState) => ({ showModal: !prevState.showModal }));
-  };
-
-  render() {
-    const { images, loading, error, showModal, largeImageURL } = this.state;
-
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.handleSearchSubmit} />
-        {error && <p>Something went wrong: {error.message}</p>}
-        <ImageGallery images={images} onImageClick={this.handleImageClick} />
-        {loading && <Loader />}
-        {images.length > 0 && !loading && <Button onClick={this.handleLoadMore} />}
-        {showModal && <Modal largeImageURL={largeImageURL} onClose={this.toggleModal} />}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <Searchbar onSubmit={handleSearchSubmit} />
+      {error && <p>Something went wrong: {error.message}</p>}
+      <ImageGallery images={images} onImageClick={handleImageClick} />
+      {loading && <Loader />}
+      {images.length > 0 && !loading && <Button onClick={handleLoadMore} />}
+      {showModal && <Modal largeImageURL={largeImageURL} onClose={toggleModal} />}
+    </div>
+  );
+};
 
 export default App;
